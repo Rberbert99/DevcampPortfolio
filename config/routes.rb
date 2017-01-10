4f07b185608f37cb24678e40958cda38a508ee76
Rberbert99 <Rberbert99@yahoo.com>
2017-01-09 21:32:17 -0700

    changed

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   resources :portfolios
-  resources :ortfolios
   get 'pages/home'
 
   get 'pages/about'
--- db/schema.rb
+++ db/schema.rb
@@ -22,16 +22,6 @@ ActiveRecord::Schema.define(version: 20170109173229) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "ortfolios", force: :cascade do |t|
-    t.string   "title"
-    t.string   "subtitle"
-    t.text     "body"
-    t.text     "main_image"
-    t.text     "thumb_image"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
-
   create_table "portfolios", force: :cascade do |t|
     t.string   "title"
     t.string   "subtitle"
