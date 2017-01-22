fdf4806285e5ae7b734f99981c030636c15e1721
Rex Berbert <rberbert99@yahoo.com>
2017-01-22 13:38:50 -0700

    Added nav bar and added default header content

---
+++ app/controllers/concerns/default_page_content.rb
@@ -0,0 +1,13 @@
+module DefaultPageContent
+  extend ActiveSupport::Concern
+
+ included do
+    before_filter :set_title
+  end
+
+  def set_title
+  	 @page_title = "Devcamp Portfolio | My Portfolio Website"
+     @seo_keywords = "Rex Portfolio"
+  end
+end
+
