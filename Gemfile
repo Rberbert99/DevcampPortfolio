05b15784f5e79647486b641c310bf05986edaa9d
Rex Berbert <rberbert99@yahoo.com>
2017-01-23 14:27:30 -0700

    Updated call to rubygems for copyright gen

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
+
 source 'https://rubygems.org'
 
 git_source(:github) do |repo_name|
@@ -55,3 +56,4 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'devise'
 gem 'bootstrap', '~> 4.0.0.alpha6'
+gem 'rex_view_tool', '~> 0.1.0'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,13 @@ class ApplicationController < ActionController::Base
 	include DeviseWhitelist
     include SetSource
     include DefaultPageContent
+
+
+   before_action :set_copyright
+
+   def set_copyright
+	@copyright = RexViewTool::Renderer.copyright 'Rex Berbert', 'All rights reserved'
+   end
 end
+
+
