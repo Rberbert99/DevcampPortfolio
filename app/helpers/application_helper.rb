814d59233bb7b1e2113831fe4a6fc2307062bbc8
Rex Berbert <rberbert99@yahoo.com>
2017-01-24 10:24:08 -0700

    Adding DeviseWhiteList

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,11 +2,11 @@ module ApplicationHelper
 
 	def login_helper
 		if current_user.is_a?(GuestUser)
+			link_to "Logout", destroy_user_session_path, method: :delete
+		else
 			(link_to "Register", new_user_registration_path) +
 			"<br>".html_safe+
 			(link_to "Login", new_user_session_path)
-		else
-			link_to "Logout", destroy_user_session_path, method: :delete
 		end
 	end
 
@@ -16,8 +16,7 @@ module ApplicationHelper
 			content_tag(:p, greeting, class: "source_greeting")
 		end
 	end
-
-	def copyright_generator
+	def copyright_generator('')
 	  RexViewTool::Renderer.copyright 'Rex Berbert'. 'All rights reserved'
 	end
 end
